6ed2d9717bb54e8c76c48a18398a1046ba6bc907
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-22 13:46:12 +0100

    Added vendor autoloader to app

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require_once  './routes.php';
+//Composer Autoload
+require_once './vendor/autoload.php';
 
 function __autoload($className) {
     require_once "./app/core/{$className}.php";
