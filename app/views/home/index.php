9bf20c7aadeb3ca3f4f0a4bc7be6085cc28aa3b0
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-22 12:10:55 +0100

    Added bootstrap library on home front end

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,25 +1,50 @@
+<!doctype html>
 <html lang="en">
     <head>
-        <title>Registration Form</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>Zoopla Crud</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
-    <body>
 
-        <h2>Registration Form</h2>
+<body>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Zoopla Crud</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="text-center">
+                <h2> All Property Listings </h2>
+            </div>
+        </div>
+
+    </div>
 
 
-        <form action="./save" method="POST"> First name:
-
-            <input type="text" name="firstname"> <br> Last name:
-
-            <input type="text" name="lastname">
-
-            <input type="hidden" name="form_submitted" value="1" />
-
-            <br />
-
-            <input type="submit" value="Submit">
-
-        </form>
-    </body>
+    <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
 </html>
