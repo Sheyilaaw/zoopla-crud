b77122f87c6cff37be865caccf5abe1f0ebc1ca2
Sheyilaaw <sheyilaaw98@gmail.com>
2019-01-23 14:47:42 +0100

    Added configuration for booting env into app

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ session_start();
 //Composer Autoload
 require_once 'vendor/autoload.php';
 
+require_once 'boot.php';
+
 require_once 'routes.php';
 require_once 'app/database.php';
+
+$sessionProvider = new EasyCSRF\NativeSessionProvider();
+$easyCSRF = new EasyCSRF\EasyCSRF($sessionProvider);
 
 $app = new App;
